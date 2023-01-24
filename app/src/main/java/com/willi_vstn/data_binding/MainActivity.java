07cf7bff1eb299ae4b1641ea7c13526b0f411c39
Willie van Staden <60608218+willivstn@users.noreply.github.com>
2023-01-24 14:41:02 +0200

    Added Databinding to layout widgets

--- app/src/main/java/com/willi_vstn/data_binding/MainActivity.java
+++ app/src/main/java/com/willi_vstn/data_binding/MainActivity.java
@@ -4,14 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingComponent;
 import androidx.databinding.DataBindingUtil;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.willi_vstn.data_binding.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-    
+
     private ActivityMainBinding activityMainBinding;
+    private MainActivityClickHandlers clickHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,15 +25,36 @@ public class MainActivity extends AppCompatActivity {
 
         Person person1 = new Person("Jack", "jack@gmail.com");
 
+        //TextView
         activityMainBinding = DataBindingUtil.setContentView(
                 this,
                 R.layout.activity_main
         );
         activityMainBinding.setPerson(person1);
 
+        //Binding the handler
+        clickHandler = new MainActivityClickHandlers(this);
+        activityMainBinding.setClickHandler(clickHandler);
+
 
     }
 
+    public class MainActivityClickHandlers{
+        Context context;
 
+        public MainActivityClickHandlers(Context context) {
+            this.context = context;
+        }
+
+        //First Button click handling
+        public void onButton1Click(View view){
+            Toast.makeText(context, "Hello!", Toast.LENGTH_SHORT).show();
+        }
+
+        //Second Button click handling
+        public void onButton2Click(View view){
+            Toast.makeText(context, "Bye!", Toast.LENGTH_SHORT).show();
+        }
+    }
 
 }
